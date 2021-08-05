287771c5e73da87b124a8d6f97b5b24746091173
Evgeni Bochkov <60480912+whitetheo83@users.noreply.github.com>
2021-08-05 17:02:34 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
+exports.max = function max (array) {
+
+//  function max (array) {
+    if (arguments.length == 0 || array.length == 0 || array == undefined) {
+      return 0;
+    } else {
+      let i;
+      let x = array[0];
+      for (i=0; i<array.length; i++) {
+        if (x<array[i]) {
+          x = array[i];
+        } else {
+          continue;
+        }
+      }
+      return(x);
+    }
+  }
 
 exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
+//  function min (array) {
+    if (arguments.length == 0 || array.length == 0 || array == undefined) {
+      return 0;
+    } else {
+      let i;
+      let x = array[0];
+      for (i=0; i<array.length; i++) {
+        if (x>array[i]) {
+          x = array[i];
+        } else {
+          continue;
+        }
+      }
+      return(x);
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+//function avg () {
+    if (arguments.length == 0 || array.length == 0 || array == undefined) {
+      return 0;
+    } else {
+      let i;
+      let x = array.length;
+      let y = 0;
+      for (i=0; i<array.length; i++) {
+        y += array[i];
+      }
+      return(y/x);
+    }
 }
+/* console.log(max([1 ,  3 ,  5 ,  7]));
+console.log(min([1 ,  3 ,  5 ,  7])); */
+//console.log(avg());
